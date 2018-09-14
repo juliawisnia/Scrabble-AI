5ee7c18c9c5fcf488b62ffb4571f6e7f7100e952
Julia Wisnia <wisnia@usc.edu>
2018-09-13 19:22:29 -0700

    new function, doesn't pass FivePlayers

--- hw2/duck_duck_goose.cpp
+++ hw2/duck_duck_goose.cpp
@@ -3,77 +3,89 @@
 #include <fstream>
 #include "duck_duck_goose.h"
 
-void simulateDDGRound(GameData * data, std::ostream & output) {
-	size_t players=data->playerList.size();
-	size_t m=rand()%4*(players-1);
-	size_t g_id=data->playerList.get(m);
-	size_t g_num=0; //goose rand number
-	size_t it_num=0; //it rand number
+void simulateDDGRound(GameData * gameData, std::ostream & output) {
+	size_t n=gameData->playerList.size();
+	size_t m=rand()%(4*(n-1)+1); // choose rand index
 
 	for (size_t i=0; i<m; i++) {
-		output<<data->playerList.get(i)<<" is a Duck."<<std::endl;
+		output<<gameData->playerList.get(i)<<" is a Duck."<<std::endl;
 	}
-	output<<data->playerList.get(m+1)<<" is a Goose!"<<std::endl;
+	output<<gameData->playerList.get(m)<<" is a Goose!"<<std::endl;
 
-	while (g_num==it_num) {
-		g_num=rand()%50;
-		it_num=rand()%50;	
+	size_t gooseNum=rand()%50;
+	size_t itNum=rand()%50;
+
+	//repick in case of tie
+	while (gooseNum==itNum) {
+		gooseNum=rand()%50;
+		itNum=rand()%50;
 	}
 
-	if (it_num>g_num) {
-		//set goose's index to 'it's' index
-		data->playerList.set(m, data->itPlayerID);
-		output<<data->itPlayerID<<" took "<<g_id<<"'s spot!"<<std::endl;
-		data->itPlayerID=g_id;
+	if (itNum>gooseNum) {
+		output<<gameData->itPlayerID<<" took "<<gameData->playerList.get(m)<<"'s spot!"<<std::endl;
+		gameData->playerList.set(m, gameData->itPlayerID);
+		gameData->itPlayerID=gameData->playerList.get(m);
 	}
 
 	else {
-		output<<data->itPlayerID<<" is out!"<<std::endl;
-		if (players==1) {
-			output<<"Winner is "<<g_id<<"!"<<std::endl;
-			data->itPlayerID=0;
+		output<<gameData->itPlayerID<<" is out!"<<std::endl;
+		//if goose is the only player left
+		if (gameData->playerList.size()==1) {
+			output<<"Winner is "<<gameData->playerList.get(m)<<"!"<<std::endl;
+			gameData->itPlayerID=0;
 		}
-		players=data->playerList.size();
+		//more players left!
+		else {
+			n=gameData->playerList.size();
+			size_t newIt=rand()%(n+1);
 
-		int new_it=rand()%players;
-		output<<data->playerList.get(new_it)<<" was chosen as the new it."<<std::endl;
-		data->playerList.remove(new_it);
+			while (newIt==m) {
+				newIt=rand()%(n+1);
+			}
+			output<<gameData->playerList.get(newIt)<<" was chosen as the new it."<<std::endl;
+			gameData->playerList.remove(newIt);
+		}
 	}
-
 }
 
 int main(int argc, char *argv[]) {
 	std::ifstream ifile;
-	if (!ifile.fail()) return -1;
+	if (argc<3) {
+		return -1;
+	}
 
 	unsigned int seed;
 	size_t players;
-	size_t it_id;
+	size_t itID;
 	size_t id;
 
 	ifile.open(argv[1]);
+	if (ifile.fail()) {
+		return -1;
+	}
 
 	ifile>>seed;
 	ifile>>players;
-	ifile>>it_id;
-
-	ifile.close();
-
-	std::filebuf fb;
-	std::ostream output(&fb);
+	ifile>>itID;
 
 	GameData* list;
-	list->itPlayerID=it_id;
-	//list->playerList.push_back(it_id);
+	list->itPlayerID=itID;
 	//create a linked list of values for players (let first it be player 0)
-	for (size_t i=0; i<players+3; i++) {
+	for (size_t i=0; i<players-1; i++) {
 		ifile>>id;
 		list->playerList.push_back(id);
 	}
 
-	srand(seed);
+	ifile.close();
 
-	simulateDDGRound(list, output);
+	srand(seed);
+	//output.open(argv[2]);
+	std::filebuf fb;
+	std::ostream output(&fb);
+
+	while (list->itPlayerID!=0) {
+		simulateDDGRound(list, output);
+	}
 
 	return 0;
 }
