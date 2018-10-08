b7eb3b36a76b888f8b8aa66ba4b0c1edf7b1c6c1
Julia Wisnia <wisnia@usc.edu>
2018-10-08 16:07:48 -0700

    run with file

--- project/src/boardTest.cpp
+++ project/src/boardTest.cpp
@@ -3,10 +3,16 @@
 #include "Board.h"
 #include "Tile.h"
 #include "Square.h"
-#include "Player.h"
 #include "ConsolePrinter.h"
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "No file specified" << std::endl;
+        return -1;
+    }
+    
     std::stringstream ss(argv[1]);
     std::string boardConfig;
     ss >> boardConfig;
