7ba1229614a01e7626f33a5a006cbf41b318b9ce
Julia Wisnia <wisnia@usc.edu>
2018-10-14 16:35:20 -0700

    didn't account for circle, rip my counter, but it works now, just less elegantly :(

--- hw5/cave.cpp
+++ hw5/cave.cpp
@@ -6,24 +6,37 @@
 int main() {
     std::string read;
     std::cin >> read;
-    int north = 0;
-    int west = 0;
+    std::stack<char> stk;
 
-    // if north, add 1, south, subtract 1
     for (size_t i = 0; i < read.size(); i++) {
         if (read[i] != 'n' && read[i] != 'e' && read[i] != 'w' && read[i] != 's') {
             std::cout << "invalid" << std::endl;
             return -1;
         }
-        
-        if (read[i] == 'n') north++;
-        else if (read[i] == 's') north--;
-        else if (read[i] == 'w') west++;
-        else if (read[i] == 'e') west--;
+
+        stk.push(read[i]);
+
+        if ((i - 1) >= 0) {
+            int j = i - 1;
+            if ((read[i] == 'n' && read[j] == 's') || (read[i] == 's' && read[j] == 'n') || (read[i] == 'w' && read[j] == 'e') || (read[i] == 'e' && read[j] == 'w')) {
+                stk.pop();
+                stk.pop();
+            }
+        }
     }
 
-    if ((north == 0) && (west == 0)) std::cout << "rolled up" << std::endl;
-    else std::cout << "not rolled up" <<std::endl;
+    char dir, dir2;
+    while (!stk.empty()) {
+        dir = stk.top();
+        dir2 = stk.top();
+        if ((dir == 's' && dir2 != 'n') || (dir == 'n' && dir2 != 's') || (dir == 'w' && dir2 != 'e') || (dir == 'e' && dir2 != 'w')) {
+            std::cout << "not rolled up " << std::endl; 
+            return 0;
+        }
+        stk.pop();
+        stk.pop();
+    }
 
+    std::cout << "rolled up" << std::endl;
     return 0;
 }
