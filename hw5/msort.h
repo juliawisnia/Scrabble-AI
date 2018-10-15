c4bb5ce7d888043f9dff0eba89afc5b2debfd0cf
Julia Wisnia <wisnia@usc.edu>
2018-10-15 14:38:57 -0700

    msort.h

--- hw5/msort.h
+++ hw5/msort.h
@@ -7,16 +7,19 @@
 
 template <class T, class Comparator>
 void merge (std::vector<T>& myArray, int l, int r, int m, Comparator comp) {
-    int i = l, j = m + 1;
+    int i = l, j = m + 1, k = 0;
     std::vector<T> temp;
     while (i <= m || j <= r) {
-        if (i <= m && (j > r || comp(myArray.at(i), myArray.at(j)))) {
-            temp.push_back(myArray.at(i));
+        //std::cout << i << " " << j << std::endl;
+        if (i <= m && (j > r || comp(myArray[i], myArray[j]) ) ) {
+            temp[k] = myArray[i];
             i++;
+            k++;
         }
         else {
-            temp.push_back(myArray.at(j));
+            temp[k] = myArray[i];
             j++;
+            k++;
         }
     }
     for (int k = 0; k < r + l - 1; k++) {
@@ -33,7 +36,7 @@ void multMergeSort(std::vector<T>& myArray, int k, int low, int high, Comparator
             multMergeSort(myArray, k, prevHigh + 1, m*i, comp);
             prevHigh = m*i;
         }
-        merge(&myArray, low, high, m, comp);
+        merge(myArray, low, high, m, comp);
     }
 }
 
@@ -42,16 +45,6 @@ void mergeSort (std::vector<T>& myArray, int k, Comparator comp) {
     int low = 0;
     int high = myArray.size() - 1;
     multMergeSort(myArray, k, low, high, comp);
-}
-
-int main() {
-    NumStrComp comp;
-    std::vector<int> array;
-    for (int i = 0; i < 10; i++) {
-        array.push_back(rand() % 100);
-    }
-    mergeSort(array, 3, comp);
-    for (int i = 0; i < 10; i++) std::cout << array[i] << std::endl;
 }
 
 
