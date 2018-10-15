bc90494cb01afd82c622acc861ac618ec3477993
Julia Wisnia <wisnia@usc.edu>
2018-10-14 17:32:26 -0700

    ok i think this is it

--- hw5/cave.cpp
+++ hw5/cave.cpp
@@ -8,27 +8,29 @@ int main() {
     std::cin >> read;
     std::stack<char> stk;
 
-    if (read.size() % 2 != 0) {
-        std::cout << "not rolled up" << std::endl;
-        return 0;
-    }
-
     for (size_t i = 0; i < read.size(); i++) {
         if (read[i] != 'n' && read[i] != 's' && read[i] != 'e' && read[i] != 'w') {
             std::cout << "invalid" << std::endl;
             return -1;
         }
 
-        if ((i + 1 < read.size()) && ((i == 'n' && i + 1 == 's') || (i == 's' && i + 1 == 'n') || (i == 'w' && i + 1 == 'e') || (i == 'e' && i + 1 == 'w'))) {
+        if ((i + 1 < read.size()) && ((read[i] == 'n' && read[i + 1] == 's') || (read[i] == 's' && read[i + 1] == 'n') 
+                                || (read[i] == 'w' && read[i + 1] == 'e') || (read[i] == 'e' && read[i + 1] == 'w'))) {
             i++;
         }
 
         else {
+            if (stk.empty()) {
+                stk.push(read[i]);
+                continue;
+            }
+
             char prev = stk.top();
-            if (!stk.empty() && ((i == 'n' && prev == 's') || (i == 's' && prev == 'n') || (i == 'w' && prev == 'e') || (i == 'e' && prev== 'w'))) {
+            if ((read[i] == 'n' && read[prev] == 's') || (read[i] == 's' && read[prev] == 'n') 
+                                || (read[i] == 'w' && read[prev] == 'e') || (read[i] == 'e' && read[prev] == 'w')) {
                 stk.pop();
             }
-            else stk.push(i);
+            else stk.push(read[i]);
         }
     }
 
