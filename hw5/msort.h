0c452ccb7e37ab9bde1fb7e97c649dc533510b0a
Julia Wisnia <wisnia@usc.edu>
2018-10-15 21:14:26 -0700

    seg fault

--- hw5/msort.h
+++ hw5/msort.h
@@ -10,7 +10,6 @@ void merge (std::vector<T>& myArray, int l, int r, int m, Comparator comp) {
     int i = l, j = m + 1, k = 0;
     std::vector<T> temp;
     while (i <= m || j <= r) {
-        //std::cout << i << " " << j << std::endl;
         if (i <= m && (j > r || comp(myArray[i], myArray[j]) ) ) {
             temp[k] = myArray[i];
             i++;
@@ -25,19 +24,29 @@ void merge (std::vector<T>& myArray, int l, int r, int m, Comparator comp) {
     for (int k = 0; k < r + l - 1; k++) {
         myArray[k + 1] = temp[k];
     }
+
+   /* for (size_t k = 0; k < temp.size(); k++) {
+        std::cout << "HI" << std::endl;
+        std::cout << temp[k] << " ";
+    }*/
+    std::cout << std::endl;
 }
 
 template <class T, class Comparator>
 void multMergeSort(std::vector<T>& myArray, int k, int low, int high, Comparator comp) {
     if (low < high) {
         int m = floor((high + low)/k);
-        //int prevHigh = -1;
-       /* for (int i = 0; i < (high - low)/k; i++) {
-            multMergeSort(myArray, k, prevHigh + 1, m*i, comp);
-            prevHigh = m*i;
-        }*/
-        multMergeSort(myArray, 2, low, m, comp);
-        multMergeSort(myArray, 2, m+1, high, comp);
+
+        std::vector<int> index((high + low)/k);
+        index[0] = low;
+        index[index.size() - 1] = high;
+        for (size_t i = 1; i < index.size() - 1; i++) {
+            if (i % 2 == 0) index[i] = m*i;
+            else index[i] = (m*i) + 1;
+        }
+        for (int i = 0; i < index.size() - 1; i++) {
+            multMergeSort(myArray, k, index[i], index[i + 1], comp);
+        }
         merge(myArray, low, high, m, comp);
     }
 }
