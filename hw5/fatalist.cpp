7936c17521fd6aea41ee01c4cb82e58135cd4cf9
Julia Wisnia <wisnia@usc.edu>
2018-10-23 09:03:11 -0700

    fatalist working, msort almost

--- hw5/fatalist.cpp
+++ hw5/fatalist.cpp
@@ -1,6 +1,6 @@
-#include "fatalist.h"
 #include "msort.h"
 #include <utility>
+#include <functional>
 #include <bits/stdc++.h>
 
 bool sortSecond (std::pair<int, int> a, std::pair<int, int> b) {
@@ -8,42 +8,18 @@ bool sortSecond (std::pair<int, int> a, std::pair<int, int> b) {
 }
 
 bool FatalistHypothesis(std::vector<std::pair<int,int> > grades) {
-    // compares CS104 grades
-    FirstComp compA;
-    // compares CS170 grades 
-    SecondComp compB;
-
     // first sort by CS104 grades, now check that CS170 grades are strictly greater
-    sort(grades, 2, compA);
+    sort(grades.begin(), grades.end());
     for (size_t i = 0; i < grades.size() - 1; i++) {
         if (grades[i].second >= grades[i + 1].second) return false;
     }
 
     // now sort by CS170 grades, and check that CS104 grades are strictly greater
-    sortSecond(grades, 2, compB);
+    sort(grades.begin(), grades.end(), sortSecond);
     for (size_t j = 0; j < grades.size() - 1; j++) {
-        if (grades[j].first >= grades[j + 1].second) return false;
+        if (grades[j].first >= grades[j + 1].first) return false;
     }
 
     // if neither function has returned, the hypothesis is true
     return true;
-}
-
-int main() {
-    std::vector<std::pair<int, int> > grades;
-    std::pair<int, int> add (90, 90);
-    grades.push_back(add);
-    std::pair<int, int> add1 (80, 80);
-    grades.push_back(add1);
-    std::pair<int, int> add2 (60, 60);
-    grades.push_back(add2);
-    std::pair<int, int> add3 (50, 50);
-    grades.push_back(add3);
-    std::pair<int, int> add4 (70, 70);
-    grades.push_back(add4);
-
-    bool a = FatalistHypothesis (grades);
-    std::cout << a << std::endl;
-    return 0;
-
 }
--- hw5/msort.h
+++ hw5/msort.h
@@ -4,61 +4,76 @@
 #include <cstdlib>
 #include <vector>
 #include <queue>
+#include <functional>
 
 template <class T, class Comparator>
 void merge (std::vector<T>& myArray, std::vector<int>& index, int m, Comparator comp) {
     std::vector<T> temp;
+    int low = index[0];
+    int high = index[index.size() - 1];
 
-    std::priority_queue<T, std::vector<T>, Comparator> pq;
-    for (size_t i = 0; i < myArray.size(); i++) {
+    // priority queue acts as pointers, only stores myArray at each index
+    // & increments the index as we merge
+    std::priority_queue<T, std::vector<T>, std::greater<int>> pq;
+    for (size_t i = 0; i < index.size(); i++) {
         pq.emplace(myArray[index[i]]);
     }
 
     while (!pq.empty()) {
         T smallest = pq.top();
+        std::cout << "smallest: " << smallest << std::endl;
         temp.push_back(smallest);
         pq.pop();
         for (size_t j = 0; j < index.size(); j++) {
             if (myArray[index[j]] == smallest) {
                 index[j]++;
+                // if one of the indicies reaches the end of that segment, break
                 if (index[j] % m == 0) break;
                 pq.emplace(myArray[index[j]]);
+                std::cout << "break" << std::endl;
                 break;
             }
         }
 
     }
 
+    if (myArray.size() == temp.size()) myArray = temp;
+
+    else {
+    	for (int i = 0; i < high - low + 1; i++) myArray[i+low] = temp[i];
+    }
+
 }
 
 template <class T, class Comparator>
 void multMergeSort(std::vector<T>& myArray, int k, int low, int high, Comparator comp) {
-    if (low < high) {
-        /*std::cout << "Low: " << low << " " << "High: " << high << std::endl;
+    if (low < high && (high - low + 1)/k > 0) {
+        std::cout << "Low: " << low << " " << "High: " << high << std::endl;
         int rem = -1;
-        if ((high - low) % k != 0) rem = (high - low) % k;
-        std::cout << "Rem: " << rem << std::endl;*/
+        if ((high - low + 1) % k != 0) rem = (high - low + 1) % k;
+        std::cout << "Rem: " << rem << std::endl;
         
-        int m = (high - low)/k;
-        //std::cout << "m: " << m << std::endl;
+        int m = (high - low + 1)/k;
+        std::cout << "m: " << m << std::endl;
         
         std::vector<int> index;
 
-        /*for (size_t i = 0; i < k; i++) {
-            index.push_back(m*i);
+        for (int i = 0; i < k; i++) {
+            index.push_back(m*i + low);
         }
 
-        for (size_t i = 0; i < index.size(); i++) std::cout << index[i] << " ";
-        std::cout << std::endl;*/
+        // if (rem > 0 && ) {
+        // 	index[index.size() - 1] -= rem;
+        // }
 
-      /*  for (int i = 0; i < index.size() - 1; i++) {
-        	std::cout << "current start is " << index[i] << " and current end is " << index[i+1] - 1 << std::endl;
-            return;
-			multMergeSort(myArray, k, index[i], index[i+1]-1, comp);
-        }*/
-        multMergeSort(myArray, k, low, m, comp);
-        multMergeSort(myArray, k, m+1, high, comp);
+        std::cout << "Indicies: ";
+        for (int i = 0; i < index.size(); i++) std::cout << index[i] << " ";
+        	std::cout << std::endl;
 
+        for (size_t i = 0; i < index.size() - 1; i++) {
+			multMergeSort(myArray, k, index[i], index[i + 1] - 1, comp);
+        }
+        multMergeSort(myArray, k, index[index.size() - 1], high, comp);
         merge(myArray, index, m, comp);
     }
 }
