5c3bf3073358cb93c1d648de4ffafa800c2e8cd1
Julia Wisnia <wisnia@usc.edu>
2018-11-27 01:43:44 -0800

    after remove prints an x at 0th position of removed letter except first one

--- hw8/Trie.cpp
+++ hw8/Trie.cpp
@@ -12,44 +12,36 @@ TrieSet::TrieSet() {
 }
 
 TrieSet::~TrieSet() {
-	//helper(root->children[0]);
+	//clearHelper(this->root);
 }
 
-void TrieSet::helper(TrieNode* node) {
-	// if (node == root && !hasChildren(node)) {
-	// 	deleteChildren(root);
-	// 	delete root;
-	// 	return;
-	// }
-
-	// // if it has no children, delete it's children and go to it's parent
-	// if (!hasChildren(node)) {
-	// 	TrieNode* temp = node->parent;
-	// 	deleteChildren(node);
-	// 	helper(temp);
-	// }
-
-	// // move on to the 'first' sequential child
-	// else {
-	// 	TrieNode* temp = node->parent;
-	// 	for (size_t i = 0; i < childrenSize; ++i) {
-	// 		helper(node->children[i]);
-	// 	}
-	// 	helper(temp);
-	// }
+void TrieSet::clearHelper(TrieNode* traverse) {
+	if (traverse != NULL) {
+		for (size_t i = 0; i < childrenSize; i++) {
+			clearHelper(traverse->children[i]);
+		}
+		delete traverse;
+	}
 }
 
 void TrieSet::insert (std::string input) {
-	TrieNode* traverse = this->root;
 	std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+	TrieNode* traverse = prefix(input);
 
+	// if it's already in the set, just set this prefix to true and return
+	if (traverse != NULL) {
+		traverse->inSet = true;
+		return;
+	}
+
+	traverse = this->root;
 	for (size_t i = 0; i < input.size(); i++) {
 		// convert to ASCII indexing
 		int index = input[i] - 97;
 		// move onto the next node, always guaranteed to have a '$' or char so won't segfault
 		traverse = traverse->children[index];
 		
-		// new unexplored path
+		// new path
 		if (traverse->letter == '$') {
 			traverse->letter = input[i];
 			insertChildren(traverse);
@@ -61,31 +53,22 @@ void TrieSet::insert (std::string input) {
 }
 
 void TrieSet::remove (std::string input) {
-	TrieNode* traverse = this->root;
 	std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+	// make sure the word is in the function
+	TrieNode* traverse = prefix(input);
+	if (traverse == NULL) return;
 
-	// make sure that the word is in it
-	for (size_t i = 0; i < input.size(); ++i) {
-		// convert to ASCII indexing
-		int index = input[i] - 97;
-		// move onto the next node, always guaranteed to have a '$' or char so won't segfault
-		traverse = traverse->children[index];
-
-		// word is not complete and is thus not in the set
-		if (traverse->letter == '$') {
-			return;
-		}
-	}
-
+	// set it to false, then see if there is any memory that we can deallocate
+	traverse->inSet = false;
 	deleteChildren(traverse);
-
+	return;
 }
 
 TrieNode* TrieSet::prefix (std::string px) {
 	TrieNode* traverse = this->root;
 	std::transform(px.begin(), px.end(), px.begin(), ::tolower);
 
-	for (size_t i = 0; i < px.size(); ++i) {
+	for (size_t i = 0; i < px.size(); i++) {
 		// convert to ASCII indexing
 		int index = px[i] - 97;
 		// move onto the next node, always guaranteed to have a '$' or char so won't segfault
@@ -102,7 +85,7 @@ TrieNode* TrieSet::prefix (std::string px) {
 }
 
 void TrieSet::insertChildren (TrieNode* node) {
-	for (size_t i = 0; i < childrenSize; ++i) {
+	for (size_t i = 0; i < childrenSize; i++) {
 		TrieNode* insertChild = new TrieNode;
 		insertChild->letter = '$';
 		insertChild->inSet = false;
@@ -119,26 +102,40 @@ bool TrieSet::hasChildren (TrieNode* node) {
 }
 
 void TrieSet::deleteChildren (TrieNode* node) {
-	while (!hasChildren(node) && node != this->root) {
+	while (!hasChildren(node) && !isRoot(node)) {
+		// if this is a word, we can't delete anymore memory so return
+		if (node->inSet) return;
+		// otherwise set this letter to the '$' and delete its children
 		node->letter = '$';
-		node->inSet = false;
-		for (size_t i = 0; i < childrenSize; ++i) {
+		// delete all it's children's memory
+		for (size_t i = 0; i < childrenSize; i++) {
 			delete node->children[i];
 		}
 		node = node->parent;
 	}
 }
 
+bool TrieSet::isRoot(TrieNode* node) {
+	if (node->letter == '&') return true;
+	return false;
+}
+
 void TrieSet::printSet() {
-//	TrieNode* traverse = this->root;
-	std::cout << "                             " << this->root->letter << std::endl;
-	for (size_t i = 0; i < 26; i++) {
-		std::cout << this->root->children[i]->letter << " ";
-	}
-	std::cout << std::endl;
-	for (size_t i = 0; i < 26; i++) {
-		std::cout << this->root->children[0]->children[i]->letter << " ";
-	}
-	std::cout << std::endl;
+	TrieNode* traverse = this->root->children[0];
+	std::cout << traverse->letter << traverse->children[0]->letter << traverse->children[3]->children[0]->letter << std::endl;
+// //	TrieNode* traverse = this->root;
+// 	std::cout << this->root->letter << std::endl;
+// 	for (size_t i = 0; i < 26; i++) {
+// 		if (this->root->children[i]->letter != '$') std::cout << i << " " << root->children[i]->letter << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	for (size_t i = 0; i < 26; i++) {
+// 		if (this->root->children[0]->children[i]->letter != '$') std::cout << i << " " << root->children[0]->children[i]->letter << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	for (size_t i = 0; i < 26; i++) {
+// 		if (this->root->children[0]->children[3]->children[i]->letter != '$') std::cout << i << " " << root->children[0]->children[3]->children[i]->letter << " ";
+// 	}
+// 	std::cout << "___________________" << std::endl;
 
 }
--- hw8/Trie.h
+++ hw8/Trie.h
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <algorithm>
+#include <stack>
+#include <set>
 #include <iostream>
 
 struct TrieNode {
@@ -13,23 +15,23 @@ struct TrieNode {
 };
 
 class TrieSet {
- public:
+  public:
 
-  TrieSet (); // a constructor for an empty trie
-  ~TrieSet (); // destructor
+    TrieSet (); // a constructor for an empty trie
+    ~TrieSet (); // destructor
 
-  void insert (std::string input);
-   /* Add this string to the set.
-      Do nothing if the string is already in the set. */
+    void insert (std::string input);
+    /* Add this string to the set.
+    Do nothing if the string is already in the set. */
 
-  void remove (std::string input);
-   /* Removes this string from the set.
-      Do nothing if the string is not already in the set. */
+    void remove (std::string input);
+    /* Removes this string from the set.
+    Do nothing if the string is not already in the set. */
 
-  TrieNode* prefix (std::string px);
-   /* returns the TrieNode matching the provided string, or returns NULL 
-      if it is not the prefix of a word in the Trie. */
-  
+    TrieNode* prefix (std::string px);
+    /* returns the TrieNode matching the provided string, or returns NULL 
+    if it is not the prefix of a word in the Trie. */
+
     // prints trieSet for debugging purposes
     void printSet();
 
@@ -38,13 +40,19 @@ class TrieSet {
     TrieNode* root;
     // function to insert 26 new children initialized with '$'
     void insertChildren(TrieNode* node);
+
     // returns false if all children are '$' meaning the end of a word
     bool hasChildren (TrieNode* node);
+
     // deletes all children of node if they are all '$', then moves on until
     // it reaches a node with children initialized to letters or the root
     void deleteChildren (TrieNode* node);
+
     // helper function that recursively deallocates all memory from the set
-    void helper(TrieNode* node);
+    void clearHelper(TrieNode* traverse);
+
+    // returns true iff node is the root
+    bool isRoot(TrieNode* node);
 
 };
 
